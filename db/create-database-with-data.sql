c210e54c0318a8b8cdd6270961bcafa9cade8171
liisa-auer <liisa.auer@oamk.fi>
2014-06-03 16:32:50 +0300

    Database scripts

--- db/create-database-with-data.sql
+++ db/create-database-with-data.sql
@@ -14,17 +14,10 @@ USE opixmanager;
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `opixuser_opix`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `contact_person`
